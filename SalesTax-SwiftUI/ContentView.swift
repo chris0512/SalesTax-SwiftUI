c3c44c17648b9075f5ea19c3cb58876f493742fc
chris0512 <kyang3200@gmail.com>
2022-06-14 17:42:53 +0800

    Initial Commit

---
+++ SalesTax-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SalesTax-SwiftUI
+//
+//  Created by Yang Chi on 2022/6/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SalesTax-SwiftUI/SalesTax_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesTax_SwiftUIApp.swift
+//  SalesTax-SwiftUI
+//
+//  Created by Yang Chi on 2022/6/14.
+//
+
+import SwiftUI
+
+@main
+struct SalesTax_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
